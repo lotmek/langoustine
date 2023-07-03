728e353dee0fd31eec98a077d792f297b3e6354a
Lotfi Meklati <lotfi.meklati@polymtl.ca>
2023-07-03 15:18:24 -0400

    fix height

--- webapp/src/components/PageContainer.tsx
+++ webapp/src/components/PageContainer.tsx
@@ -7,7 +7,7 @@ interface PageContainerProps {
 
 export const PageContainer: React.FC<PageContainerProps> = ({ children, ...props }) => {
   return (
-    <div tw="w-full min-h-full flex flex-col" {...props}>
+    <div tw="w-full h-full flex flex-col" {...props}>
       {children}
     </div>
   );
